d0999a6270e74e3d578d7da82a6595cda951c1e3
ololosh <fosepwnz@gmail.com>
2016-09-04 13:48:20 +0700

    check

---
+++ src/db/DocumentDAO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ololosh on 9/4/16.
+//
+
+#include "DocumentDAO.h"
---
+++ src/db/DocumentDAO.h
@@ -0,0 +1,14 @@
+//
+// Created by ololosh on 9/4/16.
+//
+
+#ifndef INDEXER_DOCUMENTDAO_H
+#define INDEXER_DOCUMENTDAO_H
+
+
+class DocumentDAO {
+
+};
+
+
+#endif //INDEXER_DOCUMENTDAO_H
